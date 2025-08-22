86661659547ec9886a3846fa0f740fea91611059
xcvL <baek071106@gmail.com>
2025-08-22 15:10:07 +0900

    update2

--- math/mathmetical_analysis.py
+++ math/mathmetical_analysis.py
@@ -1,9 +1,33 @@
 import functions
-from numpy import sqrt
+from general import pi, e
+from math import gamma
+from numpy import sqrt, cbrt, log, arctan
 
-#                                        name                    value
+#                                                                name                                     value
 
-pi = 3.14159265358979323846            # pi                      -
-e = 2.71828182845904523536             # Euler's number          -
-G = 1 / functions.agm(1, sqrt(2))
-print(G)
+G = 1 / functions.agm(1, sqrt(2))                                # The Gauss's constant                   0.83462684167407318628
+F = 2.80777024202851936522                                       # Fransen-Robinson constant              -
+alpha = 4.53236014182719380962                                   # Van der Pauw constant                  -
+lambda_Ch = 4 * gamma(0.25) ** 2 / pi ** ( 3 / 2 )               # Chebyshev constant                     0.59017029950804811302
+M_I = 0.07077603931152880353 - 0.684000389437932129j             # MKB constant                           -
+C_nff = 3.05940740534257614453                                   # Double factorial constant              -
+L2 = 0.2 + sqrt(25 - sqrt(20)) / pi                              # Lebesgue constant L2                   1.64218843522212113687
+C_GS = 1.11786415118994497314                                    # Goh-Schmutz constant                   -
+beta = 0.28016949902386913303                                    # Bernsteins constant                    -
+lambda_ = 0.66274341934918158097                                 # Laplace limit                          -
+I1 = 0.78343051071213440705                                      # Sophomores dream I1                    -
+I2 = 1.29128599706266354040                                      # Sophomores dream I2                    -
+W = cbrt(2.5 - sqrt(1929) / 18) + cbrt(2.5 + sqrt(1929) / 18)    # Wallis Constant                        2.09455148154232659148
+tau = 1 - 1 / e                                                  # Time constant                          0.63212055882855767840
+beta3 = 1 / 3 * (log(2) + pi / sqrt(3))                          # Baker constant                         0.83564884826472105333
+K0 = 23.1034479094205416160                                      # Kempner-Reihe Kempner Serie(0)         -
+c_KM = 1.38135644451849779337                                    # Kneser-Mahler polynomial constant      -
+P_r1 = 1.75874362795118482469                                    # Infinite product constant              -
+partial = 1.86002507922119030718                                 # Spiral of Theodorus                    -
+S5 = sqrt(21) / 2 + 0.5                                          # Nested radical S5                      2.79128784747792000329
+K_m1 = 1.74540566240734686349                                    # Khinchin harmonic mean                 -
+varpi = gamma(0.25) ** 2 / sqrt(2 * pi) * 0.5                    # Lemniscate constant                    2.62205755429211981046
+A = 1.28242712910062263687                                       # Glaisher-Kinkelin constant             -
+theta_m = arctan(sqrt(2))                                        # Magic angle                            0.955316618124509278163
+C_MRB = 0.18785964246206712024                                   # MRB constant, Marvin Ray Burns         -
+Omega = 0.56714329040978387299                                   # Omega constant, Lambert W function     -
